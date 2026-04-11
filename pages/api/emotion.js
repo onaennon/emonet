93ea29c327a604fa25193f969a073c9a507b9efe
onaennon <onaennon@gmail.com>
2026-04-10 20:44:32 -0400

    Update emotion.js

--- pages/api/emotion.js
+++ pages/api/emotion.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         contents,
-        generationConfig: { maxOutputTokens: 500, temperature: 0 }
+        generationConfig: { maxOutputTokens: 2000, temperature: 0.1 }
       })
     });
 
